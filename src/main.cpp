1931d010a4703c71f31cb10f6243093b0387b770
jirkakrepl <jiri.krepl@gmail.com>
2021-06-09 19:03:44 +0200

    feat: roomba can receive basic commands over tx->rx pin 11

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
+
 void setup() {
     // write your initialization code here
-    Serial.begin(9600);
-    Serial.println("Hello world!");
+    int rxPin=10;
+    int txPin=11;
+    SoftwareSerial Roomba(rxPin,txPin);
+    Roomba.begin(115200);
+
+    Roomba.write(128);  //Start
+    Roomba.write(132);  //Full mode
+    delay(1000);
+
+    Roomba.write(135);  //Clean
+    delay(1000);
+    Roomba.write(135);  //Clean
+
+    Roomba.write(173);  //Stop
 }
 
 void loop() {
