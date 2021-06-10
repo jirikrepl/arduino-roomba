780805049d81b878390c07d38891899b4e9b3869
jirkakrepl <jiri.krepl@gmail.com>
2021-06-10 21:20:46 +0200

    feat: get data from front bumper by RX line

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,34 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
+int rxPin=10;
+int txPin=11;
+SoftwareSerial Roomba(rxPin,txPin);
+
 void setup() {
     // write your initialization code here
-    int rxPin=10;
-    int txPin=11;
-    SoftwareSerial Roomba(rxPin,txPin);
     Roomba.begin(115200);
+    Serial.begin(9600);
 
     Roomba.write(128);  //Start
     Roomba.write(132);  //Full mode
-    delay(1000);
+    delay(100);
 
-    Roomba.write(135);  //Clean
-    delay(1000);
-    Roomba.write(135);  //Clean
+    Roomba.write(142);
+    Roomba.write(7);
+
+    while (!Roomba.available());
+    byte MSB = Roomba.read();
+    byte LSB = Roomba.read();
+    int returnVal = (MSB << 7) | LSB;
+    Serial.println("roomba returned: " + (String)returnVal);
+
+//    Roomba.write(135);  //Clean
+//    delay(500);
+//    Roomba.write(135);  //Clean
 
     Roomba.write(173);  //Stop
+    Serial.println("setup completed!");
 }
 
 void loop() {
