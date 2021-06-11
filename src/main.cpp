19e6ffbf146ad23ec63bf777612c2f43fd244ecb
jirkakrepl <jiri.krepl@gmail.com>
2021-06-11 17:38:44 +0200

    feat: readSensor function, magic numbers changed into constants

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,37 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
-int rxPin=10;
-int txPin=11;
+const int START = 128;
+const int FULL = 132;
+const int STOP = 173;
+const int SENSORS = 142;
+const int BUMPS_AND_WHEELS = 7;
+
+const int rxPin=10;
+const int txPin=11;
+
 SoftwareSerial Roomba(rxPin,txPin);
+
+byte readSensor(int packetId) {
+    Roomba.write(SENSORS);
+    Roomba.write(packetId);
+
+    while (!Roomba.available());
+    return Roomba.read();
+}
 
 void setup() {
     // write your initialization code here
     Roomba.begin(115200);
     Serial.begin(9600);
 
-    Roomba.write(128);  //Start
-    Roomba.write(132);  //Full mode
+    Roomba.write(START);
+    Roomba.write(FULL);
     delay(100);
 
-    Roomba.write(142);
-    Roomba.write(7);
+    Serial.println("roomba returned: " + (String)readSensor(BUMPS_AND_WHEELS));
 
-    while (!Roomba.available());
-    byte MSB = Roomba.read();
-    byte LSB = Roomba.read();
-    int returnVal = (MSB << 7) | LSB;
-    Serial.println("roomba returned: " + (String)returnVal);
-
-//    Roomba.write(135);  //Clean
-//    delay(500);
-//    Roomba.write(135);  //Clean
-
-    Roomba.write(173);  //Stop
+    Roomba.write(STOP);
     Serial.println("setup completed!");
 }
 
